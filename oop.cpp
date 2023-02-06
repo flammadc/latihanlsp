8a6e27c3d71ad755803543f3c193d3713e39f89b
flammadc <87932724+flammadc@users.noreply.github.com>
2023-02-06 08:33:51 +0700

    Add files via upload

--- oop.cpp
+++ oop.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
-int main(){
-	int type,ttime,price;
-	char choice;
-	bool repeat = true;
-		
-	while (repeat){
-	system ("CLS");
 
+int type,ttime,price;
+char choice;
+bool repeat = true;
+
+
+void booking() {
+	system ("CLS");
 	cout << "Program booking hotel" << endl;
 	cout << "			| Superior		| Deluxe		| Premium		" << endl;
 	cout << "========================================================================================" << endl;
@@ -24,73 +25,62 @@ int main(){
 
 	cout << "Lama menginap 			: ";
 	cin >> ttime;
-
-cout << type << endl;
-cout << ttime << endl;
 	
 	switch (type) {
 		case 1:
-	    if(1 <= ttime && ttime >= 2 ){
+			
+	    if (ttime <= 2 ) {
 	    	price = ttime * 100000;
-	    	cout << type;
-	    	cout << ttime;
-	    	cout << price;
-		} else if(3 <= ttime && ttime >= 4){
+		} else if(ttime == 3 || ttime <= 4){
 			price = ttime * 90000;
-			cout << type;
-	    	cout << ttime;
-	    	cout << price;
-		}else if(ttime >= 5){
+		} else if(ttime >= 5){
 			price = ttime * 80000; 
-				cout << type;
-	    	cout << ttime;
-	    	cout << price;
 		}
+		
 	    break;
 		
 	  case 2:
-	    if(1 <= ttime && ttime >= 2 ){
+	    if (ttime <= 2 ) {
 	    	price = ttime * 150000;
-	    	return price;
-		}
-		else if(3 <= ttime && ttime >= 4){
+		} else if(ttime == 3 || ttime <= 4){
 			price = ttime * 135000;
-			return price;
-		}
-		else if(ttime >= 5){
+		} else if(ttime >= 5){
 			price = ttime * 120000; 
-			return price;
 		}
 		break;
 	  
 	  	case 3:
-	     if(1 <= ttime && ttime >= 2 ){
+	    if (ttime <= 2 ) {
 	    	price = ttime * 200000;
-		}
-		else if(3 <= ttime && ttime >= 4){
+		} else if(ttime == 3 || ttime <= 4){
 			price = ttime * 180000;
-		}
-		else if(ttime >= 5){
+		} else if(ttime >= 5){
 			price = ttime * 160000; 
 		}
 	    break;
-	    return price;
 	  
 	}
 	
 	cout << "Total harga			: ";
 	cout << price;
 	cout << endl;
-	
-	cout << "Ingin memesan lagi? (y/n) : ";
-	cin >> choice;
-	
-	if(choice == 'Y' || choice == 'y'){
-		repeat = true;
-	}
-	if(choice == 'N' || choice == 'n'){
-		repeat = false;
-	}
-	}
-	
+	getch();
 }
+
+main(){
+    while(repeat){
+       booking();
+       cout << endl << endl;
+       cout << "Ingin memesan lagi? (y/n) : ";
+	   cin >> choice;
+		
+		if(choice == 'Y' || choice == 'y'){
+			repeat = true;
+		}
+		
+		if(choice == 'N' || choice == 'n'){
+			repeat = false;
+		}
+    }
+}
+
