8e028be825d9495af251e02a28198809a69c99b1
flammadc <87932724+flammadc@users.noreply.github.com>
2023-02-09 19:22:50 -0800

    Add files via upload

--- oop.cpp
+++ oop.cpp
@@ -1,86 +1,102 @@
 #include <iostream>
 #include <conio.h>
+#include <ctime>
+
 using namespace std;
 
-int type,ttime,price;
+int type, ttime, price, tprice;
+float tax = 0.10;
 char choice;
+string name;
 bool repeat = true;
-
-
-void booking() {
-	system ("CLS");
-	cout << "Program booking hotel" << endl;
-	cout << "			| Superior		| Deluxe		| Premium		" << endl;
-	cout << "========================================================================================" << endl;
-	cout << " 1-2 Hari		| 100.000/night		| 150.000/night		| 200.000/night		" << endl;
-	cout << " 3-4 Hari		| 90.000/night		| 135.000/night		| 180.000/night		" << endl;
-	cout << " <5 Hari		| 80.000/night		| 120.000/night		| 160.000/night		" << endl;
-	cout << "Tipe kamar" << endl;
+	
+void ui(){
+	system("cls");
+	
+	cout << "Program Menginap Di Hotel" << endl;
+	cout << "		: Superior		: Deluxe		: Premium		" << endl;
+	cout << "-------------------------------------------------------------------------------" << endl;
+	cout << "1-2 Hari	: 100.000/night		: 150.000/night		: 200.000/night		" << endl;
+	cout << "3-4 Hari	: 90.000/night		: 135.000/night		: 180.000/night		" << endl;
+	cout << ">5 Hari		: 80.000/night		: 120.000/night		: 160.000/night		" << endl;	
+	cout << "-------------------------------------------------------------------------------" << endl;
+	
+	cout << "Tipe Kamar" << endl;
 	cout << "1. Superior" << endl;
 	cout << "2. Deluxe" << endl;
 	cout << "3. Premium" << endl;
-
-	cout << "Pilih tipe kamar		: ";
-	cin >> type;
-
-	cout << "Lama menginap 			: ";
-	cin >> ttime;
+	cout << "Masukan Nama Anda		: "; cin >> name;
+	cout << "Pilih Tipe Kamar		: "; cin >> type;
+	cout << "Lama Menginap			: "; cin >> ttime;
 	
-	switch (type) {
-		case 1:
-			
-	    if (ttime <= 2 ) {
-	    	price = ttime * 100000;
+	switch(type){
+		case 1: 
+		if(ttime <= 2){
+			price = ttime * 100000;
 		} else if(ttime == 3 || ttime <= 4){
 			price = ttime * 90000;
 		} else if(ttime >= 5){
-			price = ttime * 80000; 
+			price = ttime * 80000;
 		}
+		break;
 		
-	    break;
-		
-	  case 2:
-	    if (ttime <= 2 ) {
-	    	price = ttime * 150000;
+		case 2: 
+		if(ttime <=2 ){
+			price = ttime * 150000;
 		} else if(ttime == 3 || ttime <= 4){
 			price = ttime * 135000;
 		} else if(ttime >= 5){
-			price = ttime * 120000; 
+			price = ttime * 120000;
 		}
 		break;
-	  
-	  	case 3:
-	    if (ttime <= 2 ) {
-	    	price = ttime * 200000;
+		
+		case 3: 
+		if(ttime <=2 ){
+			price = ttime * 200000;
 		} else if(ttime == 3 || ttime <= 4){
 			price = ttime * 180000;
 		} else if(ttime >= 5){
-			price = ttime * 160000; 
+			price = ttime * 160000;
 		}
-	    break;
-	  
+		break;
 	}
 	
-	cout << "Total harga			: ";
-	cout << price;
+//	REQUESTED BY HERRY
+	tprice = price * tax;
+	
+	time_t ltime = time(0);
+    
+	char* dt = ctime(&ltime);
+	string dateTime;
+	dateTime = dt;
+	dateTime.erase(dateTime.length()-1);
+	
+//	REQUESTED BY HERRY
 	cout << endl;
-	getch();
+	cout << "=================================================================" <<endl;
+	cout << "|\t\t\tHello Here's Your Receipt\t\t|" <<endl;
+	cout << "|\tFor Mr/Mrs.\t\t: " << name << "\t\t\t\t|" <<endl;
+	cout << "|\tType Room\t\t: " << type << "\t\t\t\t|" <<endl;
+	cout << "|\tTotal Days\t\t: " << ttime << "\t\t\t\t|" <<endl;
+	cout << "|\tTotal Price\t\t: " << price << "\t\t\t|" <<endl;
+	cout << "|\tTotal Price *tax\t: " << price - tprice << "\t\t\t\t|" <<endl;
+	cout << "|\tDelivered on\t\t: " << dateTime << "\t|" << endl;
+	cout << "=================================================================" <<endl;
+	
+	
 }
 
 main(){
-    while(repeat){
-       booking();
-       cout << endl << endl;
-       cout << "Ingin memesan lagi? (y/n) : ";
-	   cin >> choice;
+	while(repeat){	
+		ui();
 		
-		if(choice == 'Y' || choice == 'y'){
-			repeat = true;
-		}
-		
-		if(choice == 'N' || choice == 'n'){
-			repeat = false;
-		}
-    }
+		cout << "Ingin Memesan Lagi (Y/N)	: "; cin >> choice;
+		 if(choice == 'Y' || choice == 'y'){
+		 	repeat = true;
+		 }
+		 if(choice == 'N' || choice == 'n'){
+		 	repeat = false;
+		 }
+	}
+	
 }
-
--- pemdas.cpp
+++ pemdas.cpp
@@ -3,10 +3,10 @@
 
 using namespace std;
 
-void ascending(int nums[], int length){
-	int i,j,k;
-	for(i = 0; i < length-1; i++){
-		for(j = 0; j < length-i-1; j++){
+void ascending(int nums[]){
+	int i,j;
+	for(i = 0; i < 3-1; i++){
+		for(j = 0; j < 3-i-1; j++){
 			if(nums[j] > nums[j+1]){
 				swap(nums[j], nums[j+1]);
 			}
@@ -14,10 +14,10 @@ void ascending(int nums[], int length){
 	}
 }
 
-void descending(int nums[], int length){
-	int i,j,k;
-	for(i = 0; i < length-1; i++){
-		for(j = 0; j < length-i-1; j++){
+void descending(int nums[]){
+	int i,j;
+	for(i = 0; i < 3-1; i++){
+		for(j = 0; j < 3-i-1; j++){
 			if(nums[j] < nums[j+1]){
 				swap(nums[j], nums[j+1]);
 			}
@@ -25,59 +25,61 @@ void descending(int nums[], int length){
 	}
 }
 
-void print(int nums[], int length){
-	int i;
-	for(i = 0; i < length; i++){
-		cout << nums[i]<< " ";
-	}
-}
-
-void zerodex(int nums[], int length){
-	int i;
-	cout << nums[0] << endl;
-}
-
-void mean(int nums[], int length){
+void mean(int nums[]){
 	int i,j,k;
-	j = 0;
-	for(i = 0; i < length; i++){
-		j += nums[i];		
-		k = j / length;
+	for(i = 0; i < 3; i++){
+		j += nums[i];
+		k = j / 3;
 	}
-	cout << k;
+		cout << k;	
 }
 
-int main(){
-	int nums[3];
-	int length = sizeof(nums)/sizeof(nums[0]);
-	cout << "Masukan tiga buah nilai\n";
-	for(int i = 0; i < length; i++){
-		cout << "Nilai ke-" << i+1 << " : ";
-		cin >> nums[i];
+void zerodex(int nums []){
+	cout << nums[0];
+}
+
+void print(int nums[]){
+	int i; 
+	for(i = 0; i < 3; i++){
+		cout << nums[i] << " ";
 	}
-	
-	
-	//	start sorting section
-	ascending(nums, length);
-	cout << "Sorted Array Ascending : ";
-	print(nums, length);
-	cout << endl;
-	descending(nums, length);
-	cout << "Sorted Array Descending : ";
-	print(nums, length);
-	cout << endl;
-	//	end sorting section
-	
-	//	max
-	cout << "Nilai Max : ";
-	descending(nums, length);
-	zerodex(nums, length);
+}
 
-	cout << "Nilai Min : ";
-	ascending(nums, length);
-	zerodex(nums, length);
 
-	cout << "Nilai Mean : ";
-	mean(nums,length);
-	return 0;
+main(){
+	int i, nums[3];
+	
+	cout << "Masukan Tiga Buah Nilai" << endl;
+	cout << "Nilai A : "; cin >> nums[0];
+	cout << "Nilai B : "; cin >> nums[1];
+	cout << "Nilai C : "; cin >> nums[2];
+	
+	cout << "Urutan Nilai Ascending :";
+	ascending(nums);
+	print(nums);
+	
+	cout << endl;
+	
+	cout << "Urutan Nilai Descending :";
+	descending(nums);
+	print(nums);
+	
+	cout << endl;
+	
+	cout << "Nilai MAX : ";
+	zerodex(nums);
+	
+	cout << endl;
+	
+	cout << "Nilai MIN : ";
+	ascending(nums);
+	zerodex(nums);
+	
+	cout << endl;
+
+	
+	cout << "Nilai Rata Rata : ";
+	mean(nums);
+	
+	
 }
